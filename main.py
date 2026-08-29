8fbfe435a0dc3abc13085f4947f84accce3699e1
nny <loadedcoal@gmail.com>
2026-08-29 15:42:29 -0500

    basic mileage calculation and unloaded calculation added in using maps API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from lists import yes_words, no_words
 from dotenv import load_dotenv
 from trip_cost import TripCost
@@ -7,16 +8,62 @@ load_dotenv()
 
 api_key = os.getenv("GOOGLE_MAPS_API_KEY")
 
+OFFICE_ADDRESS = 'N5806 Co Rd M, Plymouth, WI 53073'
+
+def get_trip_miles(pick_up, drop_off):
+
+    url = "https://routes.googleapis.com/directions/v2:computeRoutes"
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": api_key,
+        "X-Goog-FieldMask": "routes.distanceMeters"
+    }
+
+    body = {
+        "origin": {
+            "address": pick_up
+        },
+        "destination": {
+            "address": drop_off
+        },
+        "travelMode": "DRIVE"
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=body
+    )
+
+    data = response.json()
+
+    rounded_miles = round(data['routes'][0]['distanceMeters'] / 1609.344)
+
+    return rounded_miles
+
+def calculate_unloaded(office, pick_up):
+
+    unloaded_pick_up = get_trip_miles(office, pick_up)
+
+    if unloaded_pick_up <= 20:
+        return 0
+    else:
+        return unloaded_pick_up
 
 while True:
+
+    pick_up_address = input('What is your pick up address?\n')
+
+    drop_off_address = input('What is your drop off address?\n')
 
     a = input('Is this trip after hours, between 5PM and 7AM? (yes or no)\n').lower()
 
     b = input('How many legs does the trip have?\n')
 
-    c = input('how many loaded miles\n')
+    c = get_trip_miles(pick_up_address, drop_off_address)
 
-    d = input('how many unloaded miles\n')
+    d = calculate_unloaded(OFFICE_ADDRESS, pick_up_address)
 
     e = input('how much wait time? (30 min intervals only)\n')
 
